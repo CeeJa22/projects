60572dbfe1838f9bd3d76feed66c1ad295b860b3
CeeJa22 <calmazan@students.kennesaw.edu>
2022-11-23 23:47:07 -0500

    New UI

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,21 +1,90 @@
 from words import word
 import random
 import string
-    
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+
+print(logo)   
 
 def hangman():
     r_comp_word = word.upper()
     guesses = set()
     win = False
     w_letters = set(r_comp_word)
-    print(r_comp_word)
+    
+    stages = ['''
+            +---+
+            |   |
+            O   |
+            /|\  |
+            / \  |
+                |
+            =========
+            ''', '''
+            +---+
+            |   |
+            O   |
+            /|\  |
+            /    |
+                |
+            =========
+            ''', '''
+            +---+
+            |   |
+            O   |
+            /|\  |
+                |
+                |
+            =========
+            ''', '''
+            +---+
+            |   |
+            O   |
+            /|   |
+                |
+                |
+            =========''', '''
+            +---+
+            |   |
+            O   |
+            |   |
+                |
+                |
+            =========
+            ''', '''
+            +---+
+            |   |
+            O   |
+                |
+                |
+                |
+            =========
+            ''', '''
+            +---+
+            |   |
+                |
+                |
+                |
+                |
+            =========
+            ''']
     
     
     print('You can quit by typing - 0')
     
     exit = '0'
     
-    lives = 10
+    stage_num = -1
+    lives = 6
     while lives != 0:
         print('You have used these letters: ', ' '.join(guesses))
         print(f'Your remaining lives = {lives}')
@@ -26,8 +95,10 @@ def hangman():
         
         guess = str(input('Enter a word or letter: \n').upper())
         if guess in guesses:
+            print(stages[stage_num])
             print('\nYou have already guessed that letter. ')
         elif guess == exit:
+            print(stages[-7])
             print('Quitter!')
             break
         elif len(guess) == 0:
@@ -37,11 +108,17 @@ def hangman():
             print(f'You have guessed {r_comp_word} correctly! ')
             win = True
         elif guess in w_letters:
+            print(stages[stage_num])
             w_letters.remove(guess)
             guesses.add(guess)    
         elif guess != r_comp_word:
+            stage_num -=1
             lives -= 1
+            print(stages[stage_num])
             guesses.add(guess)
+            if lives == 0:
+                print(stages[stage_num])
+                print(f'You lose, the word was {r_comp_word}')
         
         
            
